9e616eefa92c7fc77c38b4e2492418dc90a832f1
SoufianLagnaoui <s.lagnaoui@revotrends.com>
2021-03-13 14:40:29 +0100

    style: correct spelling

--- src/component/about.jsx
+++ src/component/about.jsx
@@ -13,8 +13,8 @@ export default class About extends Component {
                     <div className="about-desc">
                     <span className="heading-meta">About Us</span>
                     <h2 className="colorlib-heading">Who Am I?</h2>
-                    <p>I am software engineer with IT Engineer's degree, i have begin my career as fullstack engineer in startup company where i got to develop some web apps, backend for mobile apps, then i ve got a passion for Backend side, code architecture, databases, systems, proxies ... i found it it is the most interseting part of the eco-system is is challenging sometimes which gives the chance to solve problems in a clever ways.
-                    <p>What i am doing now is developing, scalling, operationg some backend apps for a company in morocco and in my free time i work on some side projects for exploring new technologies in purpose.</p>
+                    <p>I am software engineer with IT Engineer's degree, i have begin my career as fullstack engineer in a startup company where i got to develop some web apps, backend for mobile apps, then i have got a passion for the backend side, code architecture, databases, systems, proxies ... i found it the most interseting part of the eco-system it is challenging sometimes which gives me the chance to solve problems in a clever ways.
+                    <p>What i am doing now is developing, scalling, operationg some backend apps for a company in morocco and in my free time i work on some side projects in order to explore new technologies.</p>
                     </p>
                     </div>
                 </div>
