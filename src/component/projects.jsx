4316491f7deafa6660880dc46d5c717211d6e175
SoufianLagnaoui <s.lagnaoui@revotrends.com>
2021-05-25 16:03:38 +0100

    refact: add the cheify app

--- src/component/projects.jsx
+++ src/component/projects.jsx
@@ -21,7 +21,7 @@ export default class Projects extends Component {
 											<span>13 (Thirteen) is an original exciting text messaging app that is focused on connecting the world together while promoting transparency and sharing true opinions between its users.</span>
 											<span></span>
                                             <p className="icon detail">
-												<span><a href="https://www.the13app.com"><i className="icon-eye" />details</a></span>
+												<span><a href="https://www.the13app.com" target="_blank"><i className="icon-eye" />details</a></span>
 											</p>
 										</div>
 									</div>
@@ -35,7 +35,7 @@ export default class Projects extends Component {
 											<h3><a href="">Turfs (Backend)</a></h3>
 											<span>Chat, Ask Questions and Provide advise to people around you. Turfs is an app that allows you to navigate a map and chat with others near your point of interest (Turfs). Turfs could be restaurants, cafes, hospitals, etc.</span>
 											<p className="icon">
-												<span><a href="https://turfsapp.com"><i className="icon-eye" /> details</a></span>
+												<span><a href="https://turfsapp.com" target="_blank"><i className="icon-eye" /> details</a></span>
 											</p>
 										</div>
 									</div>
@@ -50,7 +50,7 @@ export default class Projects extends Component {
 											<h3><a href="">Fenders (Backend)</a></h3>
 											<span>Fenders is an app for the car owner community, which provides a forum for users in each category to exchange experiences with each other. The application also offers the possibility of organizing events for car owners.</span>
 											<p className="icon">
-												<span><a href="https://play.google.com/store/apps/details?id=com.revotrends.eliteswap"><i className="icon-eye" /> details</a></span>
+												<span><a href="https://play.google.com/store/apps/details?id=com.revotrends.eliteswap" target="_blank"><i className="icon-eye" /> details</a></span>
 											</p>
 										</div>
 									</div>
@@ -65,7 +65,7 @@ export default class Projects extends Component {
 											<h3><a href="">Bayan (Backend)</a></h3>
 											<span>With the Bayan app, you can stay informed of all events and alerts related to your area. These alerts may be issued by government agencies, bodies, or private entities. For example, you may receive alerts about weather, traffic, or important or emergency government announcements related to your area.</span>
 											<p className="icon">
-												<span><a href="https://play.google.com/store/apps/details?id=com.revotrends.eliteswap"><i className="icon-eye" /> details</a></span>
+												<span><a href="https://play.google.com/store/apps/details?id=com.revotrends.eliteswap" target="_blank"><i className="icon-eye" /> details</a></span>
 											</p>
 										</div>
 									</div>
@@ -82,7 +82,7 @@ export default class Projects extends Component {
 											<h3><a href="work.html">Cheify (Backend)</a></h3>
 											<span>Cheify is a platform that enables people to have a unique dinning experience through it they will be able to access to the most skilled chefs and coordinate with them.</span>
 											<p className="icon">
-												<span><a href="https://thechefyapp.com"><i className="icon-eye" /> details</a></span>
+												<span><a href="https://thechefyapp.com" target="_blank"><i className="icon-eye" /> details</a></span>
 											</p>
 										</div>
 									</div>
@@ -96,7 +96,7 @@ export default class Projects extends Component {
 											<span>Orange et moi, is an app that let you manage all your mobile operations, internet and landline.
                                                   It allows you to track your consumption, pay your bills, but also to recharge your number and take advantage of many customer benefits.</span>
 											<p className="icon">
-												<span><a href="https://play.google.com/store/apps/details?id=com.orange.meditel.mediteletmoi"><i className="icon-eye" /> details</a></span>
+												<span><a href="https://play.google.com/store/apps/details?id=com.orange.meditel.mediteletmoi" target="_blank"><i className="icon-eye" /> details</a></span>
 											</p>
 										</div>
 									</div>
