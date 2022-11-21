c5a5173e0f88880543457b895e0b685ea18aa73d
SoufianLagnaoui <s.lagnaoui@revotrends.com>
2022-11-21 20:30:07 +0100

    feat: add blog

--- src/component/sidebar.jsx
+++ src/component/sidebar.jsx
@@ -35,7 +35,7 @@ export default class Sidebar extends Component {
                   <li><a href="#education" data-nav-section="education">Education</a></li>
                   <li><a href="#projects" data-nav-section="projects">Projects</a></li>
                   <li><a href="#contact" data-nav-section="contact">Contact</a></li>
-                  <li><a href="http://free-tech.blog" onClick={this.handleClick}>Blog</a></li>
+                  <li><a href="http://free-tech.blog" onClick={this.handleClick}>Blogg</a></li>
                   <li></li>
                 </ul>
 
